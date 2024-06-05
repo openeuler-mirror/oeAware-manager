40bb095943d23c0bf271c633fc7ec6379925762a
fly_1997 <flylove7@outlook.com>
2024-06-05 11:08:32 +0800

    add dynamic dependencncy adjustment

--- src/plugin_mgr/instance_run_handler.cpp
+++ src/plugin_mgr/instance_run_handler.cpp
@@ -20,9 +20,8 @@ static const DataRingBuf* get_ring_buf(std::shared_ptr<Instance> instance) {
     return instance->get_interface()->get_ring_buf();
 }
 
-void InstanceRunHandler::run_instance(std::shared_ptr<Instance> instance) {
+void InstanceRunHandler::run_instance(std::vector<std::string> &deps, InstanceRun run) {
     std::vector<const DataRingBuf*> input_data;
-    std::vector<std::string> deps = instance->get_deps();
     for (size_t i = 0; i < deps.size(); ++i) {
         std::shared_ptr<Instance> ins = memory_store.get_instance(deps[i]);
         input_data.emplace_back(get_ring_buf(ins));
@@ -30,62 +29,167 @@ void InstanceRunHandler::run_instance(std::shared_ptr<Instance> instance) {
     Param param;
     param.ring_bufs = input_data.data();
     param.len = input_data.size();
-    instance->get_interface()->run(&param);
+    run(&param);
 }
 
-void InstanceRunHandler::insert_instance(std::shared_ptr<Instance> instance, uint64_t time) {
-    /* To check if an instance is enabled, enable() is called in the PluginManager. */
-    schedule_queue.push(ScheduleInstance{instance, time});
-    INFO("[PluginManager] " << instance->get_name() << " instance insert into running queue.");
-}
-
-void InstanceRunHandler::delete_instance(std::shared_ptr<Instance> instance) {
-    instance->get_interface()->disable();
-    instance->set_enabled(false);
-    INFO("[PluginManager] " << instance->get_name() << " instance delete from running queue.");
-}
-
-void InstanceRunHandler::handle_instance(uint64_t time) {
-    InstanceRunMessage msg;
-    while(this->recv_queue_try_pop(msg)){
-        std::shared_ptr<Instance> instance = msg.get_instance();
-        switch (msg.get_type()){
-            case RunType::ENABLED: {
-                insert_instance(std::move(instance), time);
-                break;
+void InstanceRunHandler::enable_instance(const std::string &name) {
+    std::queue<std::shared_ptr<Node>> instance_node_queue;
+    auto dep_handler = memory_store.get_dep_handler();
+    instance_node_queue.push(dep_handler.get_node(name));
+    std::vector<std::string> new_enabled;
+    bool enabled = true;
+    while (!instance_node_queue.empty()) {
+        auto node = instance_node_queue.front();
+        instance_node_queue.pop();
+        if (node->instance->get_enabled()) {
+            continue;
+        }
+        auto cur_name = node->instance->get_name();
+        node->instance->set_enabled(true);
+        new_enabled.emplace_back(cur_name);
+        if (!node->instance->get_interface()->enable()) {
+            enabled = false;
+            break;
+        }
+        for (auto arc = node->head->next; arc != nullptr; arc = arc->next) {
+            if (!arc->is_exist) {
+                continue;
             }
-            case RunType::DISABLED: {
-                delete_instance(std::move(instance));
-                break;
+            auto cur_node = dep_handler.get_node(arc->to);
+            in_degree[arc->to]++;
+            instance_node_queue.push(cur_node);
+        }
+    }
+    if (!enabled) {
+        for (auto &enabled_name : new_enabled) {
+            auto instance = dep_handler.get_instance(enabled_name);
+            instance->get_interface()->disable();
+            instance->set_enabled(false);
+            if (enabled_name == name) {
+                continue;
             }
+            in_degree[enabled_name]--;
+        }
+    } else {
+        for (auto &enabled_name : new_enabled) {
+            auto instance = dep_handler.get_instance(enabled_name);
+            schedule_queue.push(ScheduleInstance{instance, time});
+            INFO("[InstanceRunHandler] " << enabled_name << " instance insert into schedule queue at time " << time);
         }
     }
 }
 
-void InstanceRunHandler::schedule(uint64_t time) {
+void InstanceRunHandler::disable_instance(const std::string &name) {
+    if (in_degree[name] != 0) {
+        return;
+    }
+    std::queue<std::shared_ptr<Node>> instance_node_queue;
+    auto dep_handler = memory_store.get_dep_handler();
+    instance_node_queue.push(dep_handler.get_node(name));
+    while (!instance_node_queue.empty()) {
+        auto node = instance_node_queue.front();
+        auto &instance = node->instance;
+        instance_node_queue.pop();
+        auto cur_name = instance->get_name();
+        instance->set_enabled(false);
+        instance->get_interface()->disable();
+        INFO("[InstanceRunHandler] " << cur_name << " instance disabled at time " << time);
+        for (auto arc = node->head->next; arc != nullptr; arc = arc->next) {
+            auto cur_node = dep_handler.get_node(arc->to);
+            arc->is_exist = arc->init;
+            /* The instance can be closed only when the indegree is 0.  */
+            if (in_degree[arc->to] <= 0 || --in_degree[arc->to] != 0) {
+                continue;
+            }
+            instance_node_queue.push(cur_node);
+        }
+    }
+}
+
+void InstanceRunHandler::handle_instance() {
+    std::shared_ptr<InstanceRunMessage> msg;
+    while(this->recv_queue_try_pop(msg)){
+        std::shared_ptr<Instance> instance = msg->get_instance();
+        switch (msg->get_type()){
+            case RunType::ENABLED: {
+                enable_instance(instance->get_name());
+                break;
+            }
+            case RunType::DISABLED: {
+                disable_instance(instance->get_name());
+                break;
+            }
+        }
+        msg->notify_one();
+    }
+}
+
+void InstanceRunHandler::change_instance_state(const std::string &name, std::vector<std::string> &deps, 
+                                               std::vector<std::string> &after_deps) {
+    for (auto &dep : deps) {
+        if (std::find(after_deps.begin(), after_deps.end(), dep) != after_deps.end()) {
+            continue;
+        }
+        auto instance = memory_store.get_instance(dep);
+        if (instance == nullptr) {
+            ERROR("[InstanceRunHandler] ilegal dependency: " << dep);
+            continue;
+        }
+        /* Disable the instance that is not required.  */
+        if (instance->get_enabled()) {
+            memory_store.delete_edge(name, instance->get_name());
+            in_degree[instance->get_name()]--;
+            auto msg = std::make_shared<InstanceRunMessage>(RunType::DISABLED, instance);
+            recv_queue_push(std::move(msg));
+        }  
+    }
+    for (auto &after_dep : after_deps) {
+        if (std::find(deps.begin(), deps.end(), after_dep) != deps.end()) {
+            continue;
+        }
+        auto instance = memory_store.get_instance(after_dep);
+        if (instance == nullptr) {
+            ERROR("[InstanceRunHandler] ilegal dependency: " << after_dep);
+            continue;
+        }
+        /* Enable the instance that is required. */
+        if (!instance->get_enabled()) {
+            in_degree[instance->get_name()]++;
+            memory_store.add_edge(name, instance->get_name());
+            auto msg = std::make_shared<InstanceRunMessage>(RunType::ENABLED, instance);
+            recv_queue_push(std::move(msg));
+        }
+    }    
+}
+
+void InstanceRunHandler::schedule() {
     while (!schedule_queue.empty()) {
         auto schedule_instance = schedule_queue.top();
+        auto &instance = schedule_instance.instance;
         if (schedule_instance.time != time) {
             break;
         }
         schedule_queue.pop();
-        if (!schedule_instance.instance->get_enabled()) {
-            break;
+        if (!instance->get_enabled()) {
+            continue;
         }
-        run_instance(schedule_instance.instance);
-        schedule_instance.time += schedule_instance.instance->get_interface()->get_period();
+        std::vector<std::string> deps = instance->get_deps();
+        run_instance(deps, instance->get_interface()->run);
+        schedule_instance.time += instance->get_interface()->get_period();
         schedule_queue.push(schedule_instance);
+        /* Dynamically change dependencies. */
+        std::vector<std::string> after_deps = instance->get_deps();
+        change_instance_state(instance->get_name(), deps, after_deps);
     }
 }
 
 void start(InstanceRunHandler *instance_run_handler) {
-    unsigned long long time = 0;
     INFO("[PluginManager] instance schedule started!");
     while(true) {
-        instance_run_handler->handle_instance(time);
-        instance_run_handler->schedule(time);
+        instance_run_handler->handle_instance();
+        instance_run_handler->schedule();
         usleep(instance_run_handler->get_cycle() * 1000);
-        time += instance_run_handler->get_cycle();
+        instance_run_handler->add_time(instance_run_handler->get_cycle());
     }
 }
 

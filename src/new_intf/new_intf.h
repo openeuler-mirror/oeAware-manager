83d66e29168db21eeee6cef2cfe26cf7c66c2903
LHesperus <2639350497@qq.com>
2024-10-18 17:14:18 +0800

    a

--- src/new_intf/new_intf.h
+++ src/new_intf/new_intf.h
@@ -33,7 +33,7 @@ public:
     virtual void CloseTopic(const Topic &topic) = 0;
     virtual void UpdateData(const DataList &dataList) = 0;
     virtual std::vector<std::string> GetSupportTopics() = 0;
-    virtual bool Enable(const std::string &parma) = 0;
+    virtual int Enable() = 0;
     virtual void Disable() = 0;
     virtual void Run() = 0;
 

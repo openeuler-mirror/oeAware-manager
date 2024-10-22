9cfe532c630c4d62f847bccff0a00f04fc397edb
zhaolichang <zhaolichang@huawei.com>
2024-10-22 12:11:41 +0800

    add pmu_sampling_collector

---
+++ src/plugin/collect/pmu/pmu_sampling_collector.cpp
@@ -0,0 +1,154 @@
+/******************************************************************************
+ * Copyright (c) Huawei Technologies Co., Ltd. 2024-2024. All rights reserved.
+ * oeAware is licensed under Mulan PSL v2.
+ * You can use this software according to the terms and conditions of the Mulan PSL v2.
+ * You may obtain a copy of Mulan PSL v2 at:
+ *          http://license.coscl.org.cn/MulanPSL2
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
+ * EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
+ * MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+ * See the Mulan PSL v2 for more details.
+ ******************************************************************************/
+
+#include <algorithm>
+#include <iostream>
+#include <securec.h>
+#include "pmu_sampling_collector.h"
+#include "pmu_sampling_data.h"
+
+PmuSamplingCollector::PmuSamplingCollector(): oeaware::Interface()
+{
+    this->name = "pmu_sampling_collector";
+    this->version = "1.0.0";
+    this->description = "collect sampling information of pmu";
+    this->priority = 0;
+    this->type = 0;
+    this->period = 100;
+    for (const auto &it : topicStr) {
+        pmuId[it] = -1;
+        oeaware::Topic topic;
+        topic.instanceName = this->name;
+        topic.topicName = it;
+        supportTopics.push_back(topic);
+    }
+}
+
+void PmuSamplingCollector::InitSamplingAttr(struct PmuAttr &attr)
+{
+    attr.pidList = nullptr;
+    attr.numPid = 0;
+    attr.cpuList = nullptr;
+    attr.numCpu = 0;
+    attr.evtAttr = nullptr;
+    attr.freq = 100;
+    attr.useFreq = 1;
+    attr.excludeUser = 0;
+    attr.excludeKernel = 0;
+    attr.symbolMode = NO_SYMBOL_RESOLVE;
+    attr.callStack = 0;
+    attr.dataFilter = SPE_FILTER_NONE;
+    attr.evFilter = SPE_EVENT_NONE;
+    attr.minLatency = 0;
+    attr.includeNewFork = 0;
+}
+
+int PmuSamplingCollector::OpenSampling(const oeaware::Topic &topic)
+{
+    struct PmuAttr attr;
+    InitSamplingAttr(attr);
+
+    char *evtList[1];
+    evtList[0] = new char[topic.topicName.length() + 1];
+    errno_t ret = strcpy_s(evtList[0], topic.topicName.length() + 1, topic.topicName.c_str());
+    if (ret != EOK) {
+        std::cout << topic.topicName << " open failed, reason: strcpy_s failed" << std::endl;
+        return -1;
+    }
+    attr.evtList = evtList;
+    attr.numEvt = 1;
+
+    int pd = PmuOpen(SAMPLING, &attr);
+    if (pd == -1) {
+        std::cout << topic.topicName << " open failed" << std::endl;
+    }
+
+    return pd;
+}
+
+int PmuSamplingCollector::OpenTopic(const oeaware::Topic &topic)
+{
+    if (topic.instanceName != this->name) {
+        std::cout << "OpenTopic failed" << std::endl;
+        return -1;
+    }
+
+    for (auto &iter : topicStr) {
+        if (iter == topic.topicName) {
+            if (pmuId[iter] == -1) {
+                pmuId[iter] = OpenSampling(topic);
+                if (pmuId[iter] == -1) {
+                    std::cout << "OpenTopic failed, PmuOpen failed" << std::endl;
+                    return -1;
+                }
+                PmuEnable(pmuId[iter]);
+                timestamp = std::chrono::high_resolution_clock::now();
+                return 0;
+            }
+        }
+    }
+
+    return -1;
+}
+
+void PmuSamplingCollector::CloseTopic(const oeaware::Topic &topic)
+{
+    if (pmuId[topic.topicName] == -1) {
+        std::cout << "CloseTopic failed" << std::endl;
+    } else {
+        PmuDisable(pmuId[topic.topicName]);
+        PmuClose(pmuId[topic.topicName]);
+        pmuId[topic.topicName] = -1;
+    }
+}
+
+int PmuSamplingCollector::Enable(const std::string &parma)
+{
+    return 0;
+}
+
+void PmuSamplingCollector::Disable()
+{
+    return;
+}
+
+void PmuSamplingCollector::UpdateData(const oeaware::DataList &dataList)
+{
+    return;
+}
+
+void PmuSamplingCollector::Run()
+{
+    for (auto &iter : pmuId) {
+        if (iter.second != -1) {
+            std::shared_ptr <PmuSamplingData> data = std::make_shared<PmuSamplingData>();
+            PmuDisable(iter.second);
+            data->len = PmuRead(iter.second, &(data->pmuData));
+            PmuEnable(iter.second);
+
+            auto now = std::chrono::high_resolution_clock::now();
+            data->interval = std::chrono::duration_cast<std::chrono::milliseconds>(now - timestamp).count();
+            timestamp = now;
+
+            struct oeaware::DataList dataList;
+            dataList.topic.instanceName = this->name;
+            dataList.topic.topicName = iter.first;
+            dataList.data.push_back(data);
+            Publish(dataList);
+        }
+    }
+}
+
+void GetInstance(std::vector<std::shared_ptr<oeaware::Interface>> &interface)
+{
+    interface.emplace_back(std::make_shared<PmuSamplingCollector>());
+}
---
+++ src/plugin/collect/pmu/pmu_sampling_data.cpp
@@ -0,0 +1,107 @@
+/******************************************************************************
+ * Copyright (c) Huawei Technologies Co., Ltd. 2024-2024. All rights reserved.
+ * oeAware is licensed under Mulan PSL v2.
+ * You can use this software according to the terms and conditions of the Mulan PSL v2.
+ * You may obtain a copy of Mulan PSL v2 at:
+ *          http://license.coscl.org.cn/MulanPSL2
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
+ * EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
+ * MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+ * See the Mulan PSL v2 for more details.
+ ******************************************************************************/
+
+#include <securec.h>
+#include <iostream>
+#include "pmu_sampling_data.h"
+#include "symbol.h"
+
+oeaware::Register<PmuSamplingData> PmuSamplingData::pmuSamplingReg("pmu_sampling_collector");
+
+void PmuSamplingData::Serialize(oeaware::OutStream &out) const
+{
+    out << interval << len;
+    for (int i = 0; i < len; i++) {
+        int count = 0;
+        auto tmp = pmuData[i].stack;
+        while (tmp != nullptr) {
+            count++;
+            tmp = tmp->next;
+        }
+        out << count;
+        tmp = pmuData[i].stack;
+        while (count--) {
+            std::string module(tmp->symbol->module);
+            std::string symbolName(tmp->symbol->symbolName);
+            std::string mangleName(tmp->symbol->mangleName);
+            std::string fileName(tmp->symbol->fileName);
+            out << tmp->symbol->addr << module << symbolName << mangleName << fileName
+                << tmp->symbol->lineNum << tmp->symbol->offset << tmp->symbol->codeMapEndAddr
+                << tmp->symbol->codeMapAddr << tmp->symbol->count << tmp->count;
+            tmp = tmp->next;
+        }
+        std::string rawData(pmuData[i].rawData->data);
+        out << pmuData[i].evt << pmuData[i].ts << pmuData[i].pid << pmuData[i].tid << pmuData[i].cpu
+            << pmuData[i].cpuTopo->coreId << pmuData[i].cpuTopo->numaId << pmuData[i].cpuTopo->socketId
+            << pmuData[i].comm << pmuData[i].period << rawData;
+    }
+}
+
+void PmuSamplingData::Deserialize(oeaware::InStream &in)
+{
+    in >> interval >> len;
+    pmuData = new struct PmuData[len];
+    for (int i = 0; i < len; i++) {
+        int count;
+        in >> count;
+        pmuData[i].stack = new Stack();
+        auto tmp = pmuData[i].stack;
+        while (count--) {
+            if (count) {
+                tmp->next = new Stack();
+            }
+            tmp->symbol = new Symbol();
+            std::string module, symbolName, mangleName, fileName;
+
+            in >> tmp->symbol->addr >> module >> symbolName >> mangleName >> fileName
+            >> tmp->symbol->lineNum >> tmp->symbol->offset >> tmp->symbol->codeMapEndAddr
+            >> tmp->symbol->codeMapAddr >> tmp->symbol->count >> tmp->count;
+
+            tmp->symbol->module = new char[module.length() + 1];
+            errno_t ret = strcpy_s(tmp->symbol->module, module.length() + 1, module.c_str());
+            if (ret != EOK) {
+                std::cout << "Deserialize failed, reason: strcpy_s failed" << std::endl;
+                return;
+            }
+            tmp->symbol->symbolName = new char[symbolName.length() + 1];
+            ret = strcpy_s(tmp->symbol->symbolName, symbolName.length() + 1, symbolName.c_str());
+            if (ret != EOK) {
+                std::cout << "Deserialize failed, reason: strcpy_s failed" << std::endl;
+                return;
+            }
+            tmp->symbol->mangleName = new char[mangleName.length() + 1];
+            ret = strcpy_s(tmp->symbol->mangleName, mangleName.length() + 1, mangleName.c_str());
+            if (ret != EOK) {
+                std::cout << "Deserialize failed, reason: strcpy_s failed" << std::endl;
+                return;
+            }
+            tmp->symbol->fileName = new char[fileName.length() + 1];
+            ret = strcpy_s(tmp->symbol->fileName, fileName.length() + 1, fileName.c_str());
+            if (ret != EOK) {
+                std::cout << "Deserialize failed, reason: strcpy_s failed" << std::endl;
+                return;
+            }
+            tmp = tmp->next;
+        }
+        pmuData[i].cpuTopo = new CpuTopology();
+        std::string rawData;
+        in >> pmuData[i].evt >> pmuData[i].ts >> pmuData[i].pid >> pmuData[i].tid >> pmuData[i].cpu
+        >> pmuData[i].cpuTopo->coreId >> pmuData[i].cpuTopo->numaId >> pmuData[i].cpuTopo->socketId
+        >> pmuData[i].comm >> pmuData[i].period >> rawData;
+        pmuData[i].rawData->data = new char[rawData.length() + 1];
+        errno_t ret = strcpy_s(pmuData[i].rawData->data, rawData.length() + 1, rawData.c_str());
+        if (ret != EOK) {
+            std::cout << "Deserialize failed, reason: strcpy_s failed" << std::endl;
+            return;
+        }
+    }
+}

e9c0da0fb1b37dc0d1ec3e33959d62255030622b
LHesperus <2639350497@qq.com>
2025-04-22 20:18:25 +0800

    finish hirq tune alg first version

--- src/plugin/tune/system/network/hardirq_tune/hardirq_tune.cpp
+++ src/plugin/tune/system/network/hardirq_tune/hardirq_tune.cpp
@@ -2,6 +2,7 @@
 #include "oeaware/data/env_data.h"
 #include "oeaware/data/pmu_plugin.h"
 #include "oeaware/data/pmu_sampling_data.h"
+#include "oeaware/data/network_interface_data.h"
 
 using namespace oeaware;
 
@@ -16,6 +17,9 @@ NetHardIrq::NetHardIrq()
     subscribeTopics.emplace_back(oeaware::Topic{ OE_ENV_INFO, "static", "" });
     subscribeTopics.emplace_back(oeaware::Topic{ OE_PMU_SAMPLING_COLLECTOR, "skb:skb_copy_datagram_iovec", "" });
     subscribeTopics.emplace_back(oeaware::Topic{ OE_PMU_SAMPLING_COLLECTOR, "net:napi_gro_receive_entry", "" });
+    subscribeTopics.emplace_back(oeaware::Topic{ OE_ENV_INFO, "cpu_util", "" });
+    subscribeTopics.emplace_back(oeaware::Topic{ OE_NET_INTF_INFO, OE_NETWORK_INTERFACE_BASE_TOPIC, "operstate_up" });
+    subscribeTopics.emplace_back(oeaware::Topic{ OE_NET_INTF_INFO, OE_NETWORK_INTERFACE_DRIVER_TOPIC, "operstate_up" });
 
     description += "[introduction] \n";
     description += "               Bind the interrupt corresponding to the network interface queue to NUMA, \n";
@@ -56,7 +60,7 @@ void oeaware::NetHardIrq::UpdateSystemInfo(const DataList &dataList)
     std::string topicName = dataList.topic.topicName;
     if (topicName == "static" && !envInit) {
         EnvStaticInfo *dataTmp = static_cast<EnvStaticInfo *>(dataList.data[0]);
-        numaNuma = dataTmp->numaNum;
+        numaNum = dataTmp->numaNum;
         cpu2Numa.resize(dataTmp->cpuNumConfig);
         for (int i = 0; i < dataTmp->cpuNumConfig; i++) {
             cpu2Numa[i] = dataTmp->cpu2Node[i];
@@ -74,6 +78,70 @@ void oeaware::NetHardIrq::UpdateNetInfo(const DataList &dataList)
         netDataInterval += dataTmp->interval;
     } else if (topicName == "net:napi_gro_receive_entry") {
         UpdateQueueData(dataTmp->pmuData, dataTmp->len);
+    }
+}
+
+void NetHardIrq::UpdateEnvInfo(const DataList &dataList)
+{
+    std::string instanceName = dataList.topic.instanceName;
+    std::string topicName = dataList.topic.topicName;
+    const EnvCpuUtilParam *dataTmp = static_cast<EnvCpuUtilParam *>(dataList.data[0]);
+    if (topicName == "cpu_util") {
+        UpdateCpuInfo(dataTmp);
+    }
+}
+
+void UpdateNetBaseInfo(std::unordered_map<std::string, EthQueInfo> &ethQueData,
+    const NetworkInterfaceData *data, int dataLen)
+{
+    for (int i = 0; i < dataLen; i++) {
+        std::string dev = std::string(data[i].name);
+        ethQueData[dev].dev = dev;
+        ethQueData[dev].operstate = data[i].operstate;
+    }
+}
+
+void UpdateNetDriverInfo(std::unordered_map<std::string, EthQueInfo> &ethQueData,
+    const NetworkInterfaceDriverData *data, int dataLen)
+{
+    for (int i = 0; i < dataLen; i++) {
+        const std::string dev = std::string(data[i].name);
+        ethQueData[dev].dev = dev;
+        ethQueData[dev].busInfo = std::string(data[i].busInfo);
+    }
+}
+
+void NetHardIrq::UpdateNetIntfInfo(const DataList &dataList)
+{
+    std::string instanceName = dataList.topic.instanceName;
+    if (instanceName != OE_NET_INTF_INFO) {
+        return;
+    }
+    std::string topicName = dataList.topic.topicName;
+    if (topicName == OE_NETWORK_INTERFACE_BASE_TOPIC) {
+        const NetIntfBaseDataList *dataTmp = static_cast<NetIntfBaseDataList *>(dataList.data[0]);
+        UpdateNetBaseInfo(ethQueData, dataTmp->base, dataTmp->count);
+    } else if (topicName == OE_NETWORK_INTERFACE_DRIVER_TOPIC) {
+        const NetIntfDriverDataList *dataTmp = static_cast<NetIntfDriverDataList *>(dataList.data[0]);
+        UpdateNetDriverInfo(ethQueData, dataTmp->driver, dataTmp->count);
+    }
+}
+
+void NetHardIrq::UpdateCpuInfo(const EnvCpuUtilParam *data)
+{
+    if (data == nullptr) {
+        return;
+    }
+    if (data->dataReady == ENV_DATA_NOT_READY) {
+        return;
+    }
+    if (cpuTimeDiff.size() < data->cpuNumConfig + 1) {
+        cpuTimeDiff.resize(data->cpuNumConfig + 1);
+        cpuUtil.resize(data->cpuNumConfig + 1);
+        for (int i = 0; i < cpuTimeDiff.size(); i++) {
+            cpuTimeDiff[i].resize(CPU_UTIL_TYPE_MAX, 0);
+            cpuUtil[i].resize(CPU_UTIL_TYPE_MAX, 0);
+        }
     }
 }
 
@@ -108,11 +176,164 @@ void NetHardIrq::UpdateThreadData(const PmuData *data, int dataLen)
     }
 }
 
+void NetHardIrq::AddIrqToQueueInfo()
+{
+    for (auto &item : netQueue) {
+        const std::string &dev = item.first;
+        if (ethQueData.count(dev) == 0) {
+            continue;
+        }
+        for (auto &que : item.second) {
+            const int &queueId = que.first;
+            auto &queInfo = que.second;
+            if (ethQueData[dev].que2Irq.count(queueId) == 0) {
+                continue;
+            }
+            queInfo.irqId = ethQueData[dev].que2Irq[queueId];
+        }
+    }
+}
+
 void NetHardIrq::UpdateThreadAndQueueInfo(const RecNetQueue &queData, const RecNetThreads &thrData)
 {
-    // to do
-    (void)queData;
-    (void)thrData;
+    int queueId = queData.queueMapping;
+    int core = thrData.core;
+    int node = cpu2Numa[core];
+    QueueInfo &info = netQueue[queData.dev][queueId];
+    if (info.numaRxTimes.size() < numaNum) {
+        info.numaRxTimes.resize(numaNum, 0);
+    }
+    info.numaRxTimes[node]++;
+}
+
+void NetHardIrq::ClearInvalidQueueInfo()
+{
+    for (auto devItem = netQueue.begin(); devItem != netQueue.end(); ) {
+        auto &queueMap = devItem->second;
+        for (auto queueItem = queueMap.begin(); queueItem != queueMap.end(); ) {
+            if (queueItem->second.irqId == INVAILD_IRQ_ID) {
+                queueItem = queueMap.erase(queueItem);
+            } else {
+                ++queueItem;
+            }
+        }
+
+        if (queueMap.empty()) {
+            devItem = netQueue.erase(devItem);
+        } else {
+            ++devItem;
+        }
+    }
+}
+
+void NetHardIrq::CalCpuUtil()
+{
+    for (int cpu = 0; cpu < cpuUtil.size(); ++cpu) {
+        for (int type = 0; type < CPU_UTIL_TYPE_MAX; ++type) {
+            cpuUtil[cpu][type] = cpuTimeDiff[cpu][CPU_TIME_SUM] == 0 ?
+                0.0 : cpuTimeDiff[cpu][type] * 100.0 / cpuTimeDiff[cpu][CPU_TIME_SUM];
+        }
+    }
+}
+
+void NetHardIrq::TunePreprocessing()
+{
+    for (auto &devItem : netQueue) {
+        for (auto &queItem : devItem.second) {
+            auto &info = queItem.second;
+            int maxNode = 0, maxTimes = 0;
+            info.rxSum = 0;
+            for (size_t i = 0; i < info.numaRxTimes.size(); ++i) {
+                if (maxTimes <= info.numaRxTimes[i]) {
+                    maxTimes = info.numaRxTimes[i];
+                    maxNode = i;
+                }
+                info.rxSum += info.numaRxTimes[i];
+            }
+            info.rxMaxNode = maxNode;
+        }
+    }
+}
+
+void NetHardIrq::MigrateHardIrq()
+{
+    std::vector<HardIrqMigUint> migUint;
+    for (auto &devItem : netQueue) {
+        const std::string &dev = devItem.first;
+        for (auto &queItem : devItem.second) {
+            auto &info = queItem.second;
+            if (info.rxSum < NET_RXT_THRESHOLD) {
+                continue;
+            }
+            HardIrqMigUint unit;
+            unit.irqId = info.irqId;
+            unit.rxSum = info.rxSum;
+            unit.queId = info.queueId;
+            unit.dev = dev;
+            unit.preferredNode = info.rxMaxNode;
+            unit.lastBindCore = info.lastBindCpu;
+            unit.priority = unit.lastBindCore == INVALID_CPU_ID ? 0 : cpu2Numa[unit.lastBindCore] == unit.preferredNode;
+            migUint.emplace_back(unit);
+        }
+    }
+    // sort by rxSum
+    std::sort(migUint.begin(), migUint.end(), [](const HardIrqMigUint &a, const HardIrqMigUint &b) {
+        if (a.priority != b.priority) {
+            return a.priority > b.priority; // perferred irq which last period has bound
+        }
+        return a.rxSum > b.rxSum;
+        });
+
+    std::vector<std::vector<CpuSort>> cpuSort = SortNumaCpuUtil();
+    std::vector<int> newBindCnt(numaNum);
+    for (auto &unit : migUint) {
+        int preferredNode = unit.preferredNode;
+        if (unit.lastBindCore != -1 && preferredNode == cpu2Numa[unit.lastBindCore]) {
+            continue;
+        }
+        const std::vector<CpuSort> &sortedList = cpuSort[preferredNode];
+        /* 1. different interrupts should be allocated to different cores as much as possible
+           2. interrupt priority binding on cores with low CPU utilization */
+        int preferredCpu = sortedList[newBindCnt[preferredNode] % numaNum].cpu; // this may cause bind to the same core
+        newBindCnt[preferredNode]++;
+        if (IrqSetSmpAffinity(preferredCpu, unit.irqId)) {
+            netQueue[unit.dev][unit.queId].lastBindCpu = preferredCpu;
+        }
+    }
+}
+
+void NetHardIrq::ResetCpuInfo()
+{
+    for (auto &cpuItem : cpuTimeDiff) {
+        for (auto &item : cpuItem) {
+            item = 0;
+        }
+    }
+}
+
+std::vector<std::vector<CpuSort>> NetHardIrq::SortNumaCpuUtil()
+{
+    std::vector<std::vector<CpuSort>> rst;
+    rst.resize(numaNum);
+    for (size_t cpu = 0; cpu < cpu2Numa.size(); ++cpu) {
+        int nid = cpu2Numa[cpu];
+        // 100.0 is the max cpu util
+        rst[nid].emplace_back(cpu, 100.0 - cpuUtil[cpu][CPU_IDLE] - cpuUtil[cpu][CPU_IRQ] - cpuUtil[cpu][CPU_SOFTIRQ]);
+    }
+    for (auto &nodeCpuSort : rst) {
+        std::sort(nodeCpuSort.begin(), nodeCpuSort.end(), [](const CpuSort &a, const CpuSort &b) {
+            return a.ratio < b.ratio;
+        });
+    }
+    return rst;
+}
+
+void NetHardIrq::Tune()
+{
+    AddIrqToQueueInfo();
+    ClearInvalidQueueInfo();
+    TunePreprocessing();
+    MigrateHardIrq();
 }
 
 void NetHardIrq::MatchThreadAndQueue()
@@ -148,12 +369,18 @@ void NetHardIrq::UpdateData(const DataList &dataList)
 {
     UpdateSystemInfo(dataList);
     UpdateNetInfo(dataList);
+    UpdateEnvInfo(dataList);
+    UpdateNetIntfInfo(dataList);
 }
 
 oeaware::Result NetHardIrq::Enable(const std::string &param)
 {
     bool isActive;
     irqbalanceStatus = false;
+    if (!conf.InitConf("/lib64/oeAware-plugin/hardirq_tune.conf")) {
+        return oeaware::Result(FAILED, "read conf failed");
+    }
+
     // irq tune confilct with irqbalance
     if (ServiceIsActive("irqbalance", isActive))
     {
@@ -165,6 +392,7 @@ oeaware::Result NetHardIrq::Enable(const std::string &param)
     for (auto &topic : subscribeTopics) {
         Subscribe(topic);
     }
+    
     return oeaware::Result(OK);
 }
 
@@ -183,6 +411,11 @@ void NetHardIrq::Run()
     if (!envInit) {
         return;
     }
+    CalCpuUtil();
     MatchThreadAndQueue();
+    conf.GetEthQueData(ethQueData, conf.GetQueRegex());
+    Tune();
+    ResetCpuInfo();
     netDataInterval = 0;
+    ethQueData.clear();
 }
--- src/plugin/tune/system/network/hardirq_tune/hardirq_tune.h
+++ src/plugin/tune/system/network/hardirq_tune/hardirq_tune.h
@@ -13,8 +13,14 @@
 #define HARDIRQ_TUNE_H
 
 #include "oeaware/interface.h"
+#include "oeaware/data/env_data.h"
 #include "libkperf/pmu.h"
+#include "irq_frontend.h"
 
+namespace oeaware {
+const int INVAILD_IRQ_ID = -1;
+const int INVALID_CPU_ID = -1;
+const int NET_RXT_THRESHOLD = 10; // < 10 means infrequent networks
 struct RecNetQueue {
     uint64_t ts;
     uint64_t queueMapping;
@@ -28,8 +34,32 @@ struct RecNetThreads {
     const void *skbaddr;
 };
 
-namespace oeaware {
-class NetHardIrq : public Interface{
+struct QueueInfo {
+    int queueId;
+    int irqId = INVAILD_IRQ_ID;
+    int rxSum = 0;
+    int lastBindCpu = INVALID_CPU_ID;
+    int rxMaxNode = 0;
+    std::vector<int> numaRxTimes;
+};
+
+struct CpuSort {
+    int cpu;
+    float ratio;
+    CpuSort(int c, float r) : cpu(c), ratio(r) {}
+};
+
+struct HardIrqMigUint {
+    int irqId;
+    int rxSum;
+    int queId;
+    std::string dev;
+    int lastBindCore;
+    int preferredNode;
+    int priority;
+};
+
+class NetHardIrq : public Interface {
 public:
     NetHardIrq();
     ~NetHardIrq() override = default;
@@ -42,20 +72,37 @@ public:
 
 private:
     std::vector<oeaware::Topic> subscribeTopics;
+    std::unordered_map<std::string, std::unordered_map<int, QueueInfo>> netQueue;
     const int slidingWinLen = 200; // for match que and thread info
     bool irqbalanceStatus = false; // irqbalance status before tune
     int netDataInterval = 0; // unit ms
-    int numaNuma = 0;
+    int numaNum = 0;
     std::vector<int> cpu2Numa;
     bool envInit = false;
+    std::vector<std::vector<uint64_t>> cpuTimeDiff;
+    std::vector<std::vector<float>> cpuUtil;
     std::vector<RecNetQueue> queueData;
     std::vector<RecNetThreads> threadData;
     void UpdateSystemInfo(const DataList &dataList);
     void UpdateNetInfo(const DataList &dataList);
+    void UpdateEnvInfo(const DataList &dataList);
+    void UpdateNetIntfInfo(const DataList &dataList);
+    void UpdateCpuInfo(const EnvCpuUtilParam *data);
     void UpdateQueueData(const PmuData *data, int dataLen);
     void UpdateThreadData(const PmuData *data, int dataLen);
+    void AddIrqToQueueInfo();
     void MatchThreadAndQueue();
     void UpdateThreadAndQueueInfo(const RecNetQueue &queData, const RecNetThreads &thrData);
+    void ClearInvalidQueueInfo();
+    void CalCpuUtil();
+    void TunePreprocessing();
+    void MigrateHardIrq();
+    void ResetCpuInfo();
+    std::vector<std::vector<CpuSort>> SortNumaCpuUtil();
+    void Tune();
+
+    IrqFrontEnd conf;
+    std::unordered_map<std::string, EthQueInfo> ethQueData;
 };
 }
 
